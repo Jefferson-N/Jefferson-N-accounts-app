6f54a2feaf28f7a2bf83aa04d7a19620dae5c5b1
Jf <jeffer_12123@hotmail.com>
2026-01-13 17:11:46 -0500

    Add: Proyecto backend

---
+++ backend/accounts-backend/src/main/java/com/core/bank/accounts_backend/AccountsBackendApplication.java
@@ -0,0 +1,13 @@
+package com.core.bank.accounts.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccountsBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AccountsBackendApplication.class, args);
+	}
+
+}
---
+++ backend/accounts-backend/src/test/java/com/core/bank/accounts_backend/AccountsBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.core.bank.accounts.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccountsBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
